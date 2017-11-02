64e0e364a01ab2fd1a56c350629f6fd247cb02e3
MaximumCarnage <dylansbruton@gmail.com>
2017-11-02 19:11:07 -0400

    player controller

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+	private Rigidbody m_rb;
+	public float m_moveSpeed=1.0f;
+	public float life;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+		m_rb = gameObject.GetComponent<Rigidbody>();
+		life=3;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetAxis("Horizontal") != 0){
+			Vector3 pos = gameObject.transform.position;
+			pos.x+=Input.GetAxis("Horizontal")*m_moveSpeed*Time.deltaTime;
+			gameObject.transform.position=pos;
+	}
 	}
 }
