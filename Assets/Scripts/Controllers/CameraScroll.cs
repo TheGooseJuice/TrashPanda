ba144911543209c9944d767321ba1a903e38e7c4
MaximumCarnage <dylansbruton@gmail.com>
2017-11-02 18:32:36 -0400

    scripts

---
+++ Assets/Scripts/Controllers/CameraScroll.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScroll : MonoBehaviour {
+
+		public float m_moveSpeed = 1.0f;
+	
+	
+	void Update () {
+		Vector3 pos = gameObject.transform.position;
+		pos.z+=m_moveSpeed*/*GameManager.Instance.GameSpeed*/Time.deltaTime;
+		gameObject.transform.position=pos;
+	}
+}
