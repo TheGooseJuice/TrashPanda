e2e54b0f3746bea406ebab83c17c075e925be7f2
Brent Page <Brentpage75@gmail.com>
2017-11-02 21:26:00 -0400

    fun

---
+++ Assets/Scripts/Collision/ItemKnockback.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemKnockback : MonoBehaviour {
+     public void OnCollisionEnter(Collision other) {
+         // how much the character should be knocked back
+         var magnitude = 5000;
+         // calculate force vector
+         var force = transform.position - other.transform.position;
+         // normalize force vector to get direction only and trim magnitude
+         force.Normalize();
+         gameObject.GetComponent<Rigidbody>().AddForce(force * magnitude);
+     }
+
+}
