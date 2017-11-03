f0b697eb8a94df44e9f6d36b9b0640cc9500ac95
Brent Page <Brentpage75@gmail.com>
2017-11-02 20:36:48 -0400

    added Cats, Basic ai movement, added cat sprites, had a good time

---
+++ Assets/Scripts/Controllers/AI_Basic_Move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Basic_Move : MonoBehaviour {
+
+		public float m_moveSpeedX = -1.0f;
+
+		public float m_moveSpeedZ = -0.0f;
+	
+	
+	void Update () {
+		Vector3 pos = gameObject.transform.position;
+		pos.x+=m_moveSpeedX*/*GameManager.Instance.GameSpeed*/Time.deltaTime;
+		gameObject.transform.position=pos;
+
+		pos.z+=m_moveSpeedZ*/*GameManager.Instance.GameSpeed*/Time.deltaTime;
+		gameObject.transform.position=pos;
+	}
+}
