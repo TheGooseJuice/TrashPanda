c0fc767a6f9aace96c506b73490029c8ff477e85
MaximumCarnage <dylansbruton@gmail.com>
2017-11-08 20:27:52 -0500

    More enemy work

--- Assets/Scripts/Controllers/AI_Basic_Move.cs
+++ Assets/Scripts/Controllers/AI_Basic_Move.cs
@@ -5,27 +5,28 @@ using UnityEngine;
 public class AI_Basic_Move : MonoBehaviour {
 
 		public float m_moveSpeedX = -1.0f;
-
-		public float m_moveSpeedZ = -0.0f;
+		public GameObject m_sprite;
 		Vector3 pos=Vector3.zero;
 	
 	void Update () {
 		 pos = gameObject.transform.position;
-		pos.x+=m_moveSpeedX*/*GameManager.Instance.GameSpeed*/Time.deltaTime;
+		pos.x+=m_moveSpeedX*Time.deltaTime;
 		gameObject.transform.position=pos;
 
-		pos.z+=m_moveSpeedZ*/*GameManager.Instance.GameSpeed*/Time.deltaTime;
-		gameObject.transform.position=pos;
+	
 	}
 	void OnCollisionEnter(Collision other){
-		pos = gameObject.transform.position;
+		
+		
+
 		if(other.gameObject.tag == "Walls"){
-			if(transform.localScale.x==-5){
-				transform.localScale=new Vector3(5,5,1);
+			if(m_sprite.transform.localScale.x==-1){
+				m_sprite.transform.localScale=new Vector3(1,1,1);
 			}
 			else{
-				transform.localScale=new Vector3(-5,5,1);
+				m_sprite.transform.localScale=new Vector3(-1,1,1);
 			}
+			
 			m_moveSpeedX*=-1;
 		}
 	}
---
+++ Assets/Scripts/DownwardCats.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DownwardCats : MonoBehaviour {
+	public float m_moveSpeedZ = -0.0f;
+	Vector3 pos;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	 pos = gameObject.transform.position;
+		pos.z+=m_moveSpeedZ*Time.deltaTime;
+		gameObject.transform.position=pos;
+	}
+}
